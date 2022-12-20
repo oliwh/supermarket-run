2e7c31e3f393b40ae71aa531e474eb80ee71752b
Oliver Woodhouse <oliverwoodhouse06@gmail.com>
2022-12-20 15:03:18 +0000

    various additions - added movement on keypressed - draw shelves - added scalar system to make everything grid-based

--- sketch.js
+++ sketch.js
@@ -1,12 +1,39 @@
-function preload() {
+import Player from 'Player.js';
+import 'initShelves.js';
 
+let player;
+let scale = 15;
+
+
+function preload() {
 }
 
 function setup() {
-    createCanvas(800, 600);
-    background('#62adca');
+    frameRate(60);
+    createCanvas(scale * 25, scale * 25);
+    player = new Player(13, 12);
+    //instanciates all shelves
+    initShelves();
 }
 
 function draw() {
-    
+    background('#9a9fa7');
+    player.show();
+    //draws all shelves
+    for(let i = 0; i < shelves.length; i++) {
+        shelves[i].show();
+    }
 }
+//moves the player when wasd are pressed
+function keyPressed() {
+    if(key == 'w') {
+        player.move(0, -1);
+    } else if(key == 's') {
+        player.move(0, 1);
+    } else if(key == 'a') {
+        player.move(-1, 0);
+    } else if(key == 'd') {
+        player.move(1, 0);
+    }
+}    
+
