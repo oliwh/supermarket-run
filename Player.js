d0daa92e29f0d3cbd418304e1a1e39269f85f6f6
Oliver Woodhouse <oliverwoodhouse06@gmail.com>
2022-12-22 14:12:26 +0000

    redesigned systems - redesigned collision system - redesigned movement system

--- Player.js
+++ Player.js
@@ -2,26 +2,43 @@ class Player {
     constructor(x, y) {
         this.x = levelScale * x;
         this.y = levelScale * y;
+        this.size = createVector(levelScale / 2, levelScale / 2)
+        print("size: " + this.size);
+        this.velocity = 3;
     }
 
     move(x, y) {
-        if((this.x + (levelScale * x) <= levelScale * 24) && (this.x + (levelScale * x) >= 0) && (this.y + (levelScale * y) <= levelScale * 24) && (this.y + (levelScale * y) >= 0)) {
-            if(!(level[(this.y + (levelScale * y))/levelScale][(this.x + (levelScale * x))/levelScale] == 1)) {
-                this.x += levelScale * x;
-                this.y += levelScale * y;
-            }
-            print(this.x);
-            print(this.y);
+        if(!this.collision(x, y)) {
+            this.x += x * this.velocity;
+            this.y += y * this.velocity;
         }
     }
 
     show() {
         //placeholder character
-        stroke(255);
-        strokeWeight(4);
-        noFill();
-        ellipseMode(CORNER);
-        ellipse(this.x, this.y, levelScale, levelScale);
+        push();
+        noStroke();
+        fill(255, 0, 0);
+        ellipseMode(CENTER);
+        ellipse(this.x + (levelScale / 2), this.y + (levelScale / 2), this.size.x, this.size.y);
+        pop();
         //image(playerImg, this.x, this.y, 88, 192);
+    }
+
+    collision(x, y) {
+        for(let i = 0; i < tiles.length - 1; i++) {
+            if((this.x + (this.size.x / 2) + (x * this.velocity) > tiles[i].x - (levelScale / 2)) &&
+               (this.x - (this.size.x / 2) + (x * this.velocity) < tiles[i].x + (levelScale / 2)) &&
+               (this.y + (this.size.y / 2) + (y * this.velocity) > tiles[i].y - (levelScale / 2)) &&
+               (this.y - (this.size.y / 2) + (y * this.velocity) < tiles[i].y + (levelScale / 2)) ||
+               (this.x + (this.size.x / 2) + (x * this.velocity) > levelScale * 25 - this.size.x) ||
+               (this.x - (this.size.x / 2) + (x * this.velocity) < -this.size.x) ||
+               (this.y + (this.size.y / 2) + (y * this.velocity) > levelScale * 25 - this.size.y) ||
+               (this.y - (this.size.y / 2) + (y * this.velocity) < - this.size.y)) {
+                print("collision at: " + tiles[i].x + ", " + tiles[i].y);
+                return true;
+            } 
+        }
+        return false;
     }
 }
